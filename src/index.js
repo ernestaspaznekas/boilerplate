24baa9660c1ba5a79b2aa6c3f6b5a4e0df3196e9
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2017-11-16 18:12:15 +0200

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './img.png'
-import Data from './data.xml'
+import printMe from './print'
 
 function component() {
-    var element = document.createElement('div');
+    const element = document.createElement('div')
+    const btn = document.createElement('button')
   
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
 
-    let myIcon = new Image()
-    myIcon.src = Icon
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
 
-    element.appendChild(myIcon)
-
-    console.log(Data)
-
+    element.appendChild(btn)
+    
     return element
 }
   
