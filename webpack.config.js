fa93f07e328c2b44cc6177036c86bf45bab4b87d
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2017-11-16 18:24:21 +0200

    HtmlWebpackPlugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
       app: './src/index.js',
       print: './src/print.js'
   },
+  plugins:[
+      new HtmlWebpackPlugin({
+          // And any other config options from html-webpack-plugin:
+          // https://github.com/ampedandwired/html-webpack-plugin#configuration
+          title: 'Output Management'
+      })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
